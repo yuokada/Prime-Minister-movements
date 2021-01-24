ed7eb17e629f1b8cc5fbb8be295d3f6ed264627e
Yukihiro Okada <yuokada@users.noreply.github.com>
2021-01-24 14:08:12 +0900

    Add a custom task in build.sbt

---
+++ src/main/scala/CurrentDate.scala
@@ -0,0 +1,50 @@
+import java.net.URI
+import java.net.http.HttpClient.{Redirect, Version}
+import java.net.http.{HttpClient, HttpRequest, HttpResponse}
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files.newBufferedWriter
+import java.nio.file.Paths
+import java.time.format.DateTimeFormatter
+import java.time.{Duration, ZoneId}
+
+object CurrentDate {
+
+  @deprecated
+  def downloadCSV(url: String): Option[String] = {
+    // see: https://docs.oracle.com/javase/jp/11/docs/api/java.net.http/java/net/http/HttpClient.html
+    val client = HttpClient
+      .newBuilder()
+      .version(Version.HTTP_1_1)
+      .followRedirects(Redirect.NORMAL)
+      .connectTimeout(Duration.ofSeconds(20))
+      .build();
+
+    val request = HttpRequest
+      .newBuilder()
+      .GET()
+      .uri(URI.create(url))
+      .timeout(Duration.ofSeconds(10))
+      .build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    response.statusCode() match {
+      case 200 => {
+        Option(response.body())
+      }
+      case _ => {
+        None
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val today        = java.time.LocalDate.now(ZoneId.of("Asia/Tokyo"))
+    val yearAndMonth = today.format(DateTimeFormatter.ofPattern("YYYYMM"))
+
+    val url      = s"https://www.nhk.or.jp/politics/souri/csv/${yearAndMonth}.csv"
+    val response = scala.io.Source.fromURL(url)(StandardCharsets.UTF_16)
+
+    val writer = newBufferedWriter(Paths.get(s"original/${yearAndMonth}.csv"), StandardCharsets.UTF_8)
+    response.mkString.split("\n").foreach(line => writer.write(line.stripTrailing() + "\n"))
+    writer.close()
+  }
+}
